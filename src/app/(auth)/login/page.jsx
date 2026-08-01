20edb61332e850dde9d0253866dfbde23220a978
tareq-360 <mdtareqhossain2002@gmail.com>
2026-08-01 20:28:44 +0600

    email and password register and login functionality done

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,21 +1,43 @@
+'use client'
+import { authClient } from "@/app/lib/auth-client";
 import Link from "next/link";
+import { Form, useForm } from "react-hook-form";
 
 const Login = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = async(data) => {
+        const {email,password}=data;
+        const { data: res, error } = await authClient.signIn.email({
+            email: email, // required
+            password: password, // required
+            rememberMe: true,
+            callbackURL: "/",
+        });
+    }
     return (
         <div className=" container flex justify-center items-center mx-auto min-h-screen">
             <div>
-                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-                    <h1 className=" text-xl font-bold text-center py-5">Login Your Account</h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                        <h1 className=" text-xl font-bold text-center py-5">Login Your Account</h1>
 
-                    <label className="label">Email</label>
-                    <input type="email" className="input" placeholder="Email" />
+                        <label className="label">Email</label>
+                        <input {...register("email", { required: "email field is empty!" })} type="email" className="input text-white" placeholder="Email" />
+                        {errors.email && <span className=" text-red-500 font-bold p-2">{errors.email.message}</span>}
+                        <label className="label">Password</label>
+                        <input {...register("password", { required: "You have to give password!" })} type="password" className="input text-white" placeholder="Password" />
+                        {errors.password && <span className=" text-red-500 font-bold p-2">{errors.password.message}</span>}
+                        <button className="btn btn-neutral mt-4">Login</button>
+                        <p className=" p-2">Don't You Have Account? <span className=" text-blue-600 font-bold"><Link href="/register">Register</Link></span></p>
+                    </fieldset>
+                </form>
 
-                    <label className="label">Password</label>
-                    <input type="password" className="input" placeholder="Password" />
-
-                    <button className="btn btn-neutral mt-4">Login</button>
-                    <p className=" p-2">Don't You Have Account? <span className=" text-blue-600 font-bold"><Link href="/register">Register</Link></span></p>
-                </fieldset>
             </div>
         </div>
     );
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,21 +1,58 @@
+'use client'
+import { authClient } from "@/app/lib/auth-client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
+    const router = useRouter();
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = async (data) => {
+        const { name, email, password } = data;
+        // console.log("Name = ",name);
+        const { data: res, error } = await authClient.signUp.email({
+            name: name, // required
+            email: email, // required
+            password: password, // required
+            // image: "https://example.com/image.png",
+            callbackURL: "/login",
+        });
+        // console.log(res? `data = ${res}` : `error= ${error.message}`);
+        if(res){
+            alert("Register Successful ")
+            router.push("/login");
+        }
+        else{
+            alert(error.message)
+        }
+        // {res? alert("Register Successful ") : alert(error.message)};
+        // console.log("Password ",password);
+    }
     return (
         <div className=" container flex justify-center items-center mx-auto min-h-screen">
             <div>
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
                         <h1 className=" text-xl font-bold text-center py-5">Register Your Account</h1>
 
                         <label className="label">Name</label>
-                        <input type="text" className="input" placeholder="Your Name " />
+                        <input {...register("name", { required: "name field is empty!" })} type="text" className="input text-white" placeholder="Your Name " />
+                        {errors.name && <span className=" text-red-500 font-bold p-2">{errors.name.message}</span>}
 
                         <label className="label">Email</label>
-                        <input type="email" className="input" placeholder="Email" />
+                        <input {...register("email", { required: "email field is empty!" })} type="email" className="input text-white" placeholder="Email" />
+                        {errors.email && <span className=" text-red-500 font-bold p-2">{errors.email.message}</span>}
 
                         <label className="label">Password</label>
-                        <input type="password" className="input" placeholder="Password" />
+                        <input {...register("password", { required: "password field is empty!" })} type="password" className="input text-white" placeholder="Password" />
+                        {errors.password && <span className=" text-red-500 font-bold p-2">{errors.password.message}</span>}
 
                         <button className="btn btn-neutral mt-4">Register</button>
                         <p className=" p-2">Do You Have Account? <span className=" text-blue-600 font-bold"><Link href="/login">Login</Link></span></p>
