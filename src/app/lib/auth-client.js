c87b34477be46bb9b398d99cf0a6320f198edd52
tareq-360 <mdtareqhossain2002@gmail.com>
2026-08-02 19:17:55 +0600

    url change and add vercel live url

--- src/app/lib/auth-client.js
+++ src/app/lib/auth-client.js
@@ -1,7 +1,8 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    // baseURL: "http://localhost:3000"
+    baseURL: "https://book-borrowing-platform-seven.vercel.app/"
 })
 
 export const { signIn, signUp,signOut, useSession } = createAuthClient();
